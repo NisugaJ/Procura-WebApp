0f26871c4f9a73c17295a2fa4d3536192bfeef58
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2020-10-14 20:57:21 +0530

    added orders list

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -44,9 +44,8 @@ export default function Admin({ ...rest }) {
   // ref to help us initialize PerfectScrollbar on windows devices
   const mainPanel = React.createRef();
   // states and functions
-  const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("blue");
-  const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
+  const [image] = React.useState(bgImage);
+  const [color] = React.useState("blue");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   // const handleImageClick = (image) => {
   //   setImage(image);
